35e406781c9a3cd5819b33be42d8b8adbf91a111
AugustHunnerup <august.hunnerup@gmail.com>
2024-11-01 19:21:48 +0100

    Home page

--- HotelWEBAPP/obj/Debug/net8.0/HotelWEBAPP.AssemblyInfo.cs
+++ HotelWEBAPP/obj/Debug/net8.0/HotelWEBAPP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelWEBAPP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0becb9e28d6fed8db0f63f9d572768bfcf2d669b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bcb3109eceacbaaf44374122ff4748a4ec50424")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelWEBAPP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelWEBAPP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
